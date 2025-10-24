42bf9661f37f4dd85187df68cb64208755c71e66
azimkhoja1 <azimkhoja@gmail.com>
2025-10-24 12:24:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
-import fetch from "node-fetch";   // keep this line same (now using v2.6.7)
+import fetch from "node-fetch";
 import { getBitgetTicker, getTopMarkets } from "./bitget.js";
 import { RSI, MACD, EMA } from "technicalindicators";
+
 
 const app = express();
 app.use(cors());
@@ -138,4 +139,5 @@ app.post("/api/mode", (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 DexScan PRO backend running on port ${PORT}`);
 });
+
 
